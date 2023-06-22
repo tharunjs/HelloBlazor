140046ae36cd7c9aded28036145e3217cf191621
Tharunavignesh <tharunav@nalashaa.com>
2023-06-22 17:48:21 +0530

    localization

--- HelloBlazorServerApp/Program.cs
+++ HelloBlazorServerApp/Program.cs
@@ -10,11 +10,27 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddControllers();
 builder.Services.AddMemoryCache();
 //builder.Services.AddSingleton<IMemoryCache>();
 builder.Services.AddSingleton<WeatherForecastService>();
 
+builder.Services.AddLocalization();
+
+
+
+
+
+
 var app = builder.Build();
+
+var supportedCultures = new[] { "en-US", "fr" };
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -30,6 +46,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
